fd9efbee50d0f32d705cbb70240cf5c732bb9755
sandeeproy540 <sandeeproy540@gmail.com>
2025-09-17 12:54:40 +0530

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,17 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source = "hashicorp/azurerm"
+      version = "4.44.0"
+    }
+  }
+}
+
+provider "azurerm" {
+  # Configuration options
+}
+
+resource "azurerm_resource_group" "example" {
+  name     = "example"
+  location = "West Europe"
+}
