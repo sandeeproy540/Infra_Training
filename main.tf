f717245df722a1b39429ba8da5bfdfe475741d98
sandeeproy540 <sandeeproy540@gmail.com>
2025-09-17 14:26:17 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,4 +1,10 @@
+# Create a random name for the resource group using random_pet
+resource "random_pet" "rg_name" {
+  prefix = var.resource_group_name_prefix
+}
+
+# Create a resource group using the generated random name
 resource "azurerm_resource_group" "example" {
-  name     = "First_RG"
-  location = "West Europe"
+  location = var.resource_group_location
+  name     = random_pet.rg_name.id
 }
